a623c8690289a0349b3fa59a0c793b524e76ec2b
DenisNuzhnykh <dnuzhnykh@mail.ru>
2020-05-31 20:04:59 +0500

    Сделал третью лабу

---
+++ 3/lab_3.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Person {
+    int age;
+    string name;
+    char value;
+};
+
+Person createPerson(string name, int age, char value){
+    Person newPerson;
+    newPerson.age = age;
+    newPerson.name = name;
+    newPerson.value = value;
+    return newPerson;
+}
+
+int main()
+{
+    Person newPerson = createPerson("Denis",20,'c');
+    cout  << newPerson.age << endl;
+    cout  << newPerson.name << endl;
+    cout  << newPerson.value << endl;
+    
+}
